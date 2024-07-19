Fixed masking (no random replacement, no special token masking)

src/dataloaders/genomics.py
@@ -52,7 +52,7 @@ class TCGADataset(torch.utils.data.Dataset):
         random.Random(42).shuffle(self.pat_list)
         self.seqs_per_pat = seqs_per_pat
         self.tokenizer = tokenizer
-        
+        logger.info(f"{self.seqs_per_pat} will be loaded per patient.")
         self.mlm = mlm
         self.mlm_probability = mlm_probability
         if self.mlm and self.mlm_probability <= 0.0:
@@ -66,6 +66,7 @@ class TCGADataset(torch.utils.data.Dataset):
         else:
             self.eligible_replacements = None
         self.max_length = max_length
+        logger.info(f"{self.max_length=}")
         self.pad_max_length = pad_max_length if pad_max_length is not None else max_length
         self.tokenizer_name = tokenizer_name
         self.tokenizer = tokenizer

src/dataloaders/utils/mlm.py
@@ -13,20 +13,33 @@ def mlm_getitem(seq, mlm_probability=0.15, contains_eos=False, tokenizer=None, e
     probability_matrix = torch.full(target.shape, mlm_probability)
     # TODO: Do we need to avoid "masking" special tokens as is done here?
     #  https://github.com/huggingface/transformers/blob/14666775a296a76c88e1aa686a9547f393d322e2/src/transformers/data/data_collator.py#L760-L766
+    
+    #if special_tokens_mask is None:
+    #TODO adjust for batch size > 1 case! #FIXME
+    special_tokens_mask = tokenizer.get_special_tokens_mask(target, already_has_special_tokens=True) 
+    special_tokens_mask = torch.tensor(special_tokens_mask, dtype=torch.bool)
+    #else:
+    #    special_tokens_mask = special_tokens_mask.bool()
+
+    probability_matrix.masked_fill_(special_tokens_mask, value=0.0)
     masked_indices = torch.bernoulli(probability_matrix).bool()
+    
+    #masked_indices = torch.bernoulli(probability_matrix).bool()
     target[~masked_indices] = tokenizer.pad_token_id  # We only compute loss on masked tokens
 
     # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
-    indices_replaced = torch.bernoulli(torch.full(target.shape, 0.8)).bool() & masked_indices
-    data[indices_replaced] = tokenizer.convert_tokens_to_ids(tokenizer.mask_token)
+    #indices_replaced = torch.bernoulli(torch.full(target.shape, 0.8)).bool() & masked_indices
+    #data[indices_replaced] = tokenizer.convert_tokens_to_ids(tokenizer.mask_token)
 
+    data[masked_indices] = tokenizer.convert_tokens_to_ids(tokenizer.mask_token)
+    
     # 10% of the time, we replace masked input tokens with random word
-    indices_random = torch.bernoulli(torch.full(target.shape, 0.5)).bool() & masked_indices & ~indices_replaced
-    if eligible_replacements is not None:
-        rand_choice = torch.randint(eligible_replacements.shape[0], size=target.shape)
-        random_words = eligible_replacements[rand_choice]
-    else:
-        random_words = torch.randint(len(tokenizer), size=target.shape, dtype=torch.long)
-    data[indices_random] = random_words[indices_random]
+    #indices_random = torch.bernoulli(torch.full(target.shape, 0.5)).bool() & masked_indices & ~indices_replaced
+    #if eligible_replacements is not None:
+    #    rand_choice = torch.randint(eligible_replacements.shape[0], size=target.shape)
+    #    random_words = eligible_replacements[rand_choice]
+    #else:
+    #    random_words = torch.randint(len(tokenizer), size=target.shape, dtype=torch.long)
+    #data[indices_random] = random_words[indices_random]
     # The rest of the time (10% of the time) we keep the masked input tokens unchanged
     return data, target