fb9f98b76f6d3734383fdcc3ee183ca678f0ef4d
leannmlindsey <leannmlindsey@gmail.com>
2024-09-19 06:44:26 -0600

    changed wandb group to phage

--- slurm_scripts/wrapper_run_phage.sh
+++ slurm_scripts/wrapper_run_phage.sh
@@ -26,7 +26,7 @@
 #LRS=( "6e-4" "6e-5")
 
 ## Mamba NTP
-LOG_DIR="../watch_folder/phage_cv10/mamba_08182024"
+#LOG_DIR="../watch_folder/phage_cv10/mamba_08182024"
 # model comparison 1
 #CONFIG_PATH="/uufs/chpc.utah.edu/common/home/u1323098/sundar-group-space2/PHAGE_FINAL_PAPER/MODELS/TEST_BED/caduceus/outputs/pretrain/hg38/mamba_ntp_rc_aug_seqlen-4k_d_model-128_n_layer-4_lr-8e-5/model_config.json"
 #PRETRAINED_PATH="/uufs/chpc.utah.edu/common/home/u1323098/sundar-group-space2/PHAGE_FINAL_PAPER/MODELS/TEST_BED/caduceus/outputs/pretrain/hg38/mamba_ntp_rc_aug_seqlen-4k_d_model-128_n_layer-4_lr-8e-5/checkpoints/last.ckpt"
@@ -41,12 +41,12 @@ LOG_DIR="../watch_folder/phage_cv10/mamba_08182024"
 #CONFIG_PATH="/uufs/chpc.utah.edu/common/home/u1323098/sundar-group-space2/PHAGE_FINAL_PAPER/MODELS/TEST_BED/caduceus/outputs/pretrain/hg38/mamba_ntp_rc_aug_seqlen-4k_d_model-256_n_layer-8_lr-8e-5/model_config.json"
 #PRETRAINED_PATH="/uufs/chpc.utah.edu/common/home/u1323098/sundar-group-space2/PHAGE_FINAL_PAPER/MODELS/TEST_BED/caduceus/outputs/pretrain/hg38/mamba_ntp_rc_aug_seqlen-4k_d_model-256_n_layer-8_lr-8e-5/checkpoints/last.ckpt"
 #DISPLAY_NAME="mamba_bpe_4k_d256_8L"
-MODEL="mamba"
-MODEL_NAME="dna_embedding_mamba"
-CONJOIN_TRAIN_DECODER="false"
-CONJOIN_TEST="false"
-RC_AUGS=( "true" )
-LRS=( "1e-5" "1e-4")
+#MODEL="mamba"
+#MODEL_NAME="dna_embedding_mamba"
+#CONJOIN_TRAIN_DECODER="false"
+#CONJOIN_TEST="false"
+#RC_AUGS=( "true" )
+#LRS=( "1e-5" "1e-4")
 
 ## Caduceus NO POST HOC
 #LOG_DIR="../watch_folder/gb_cv5/caduceus"
@@ -85,27 +85,21 @@ LRS=( "1e-5" "1e-4")
 #LRS=( "1e-3" "2e-3" )
 
 ## Caduceus Parameter Sharing
-#LOG_DIR="../watch_folder/nt_cv10_ep20/caduceus"
-#CONFIG_PATH=$(realpath "../outputs/pretrain/hg38/caduceus-ps_seqlen-1k_d_model-256_n_layer-4_lr-8e-3/model_config.json")
-#PRETRAINED_PATH=$(realpath "../outputs/pretrain/hg38/caduceus-ps_seqlen-1k_d_model-256_n_layer-4_lr-8e-3/checkpoints/last.ckpt")
-#DISPLAY_NAME="caduceus_ps"
-#CONFIG_PATH="/uufs/chpc.utah.edu/common/home/u1323098/sundar-group-space2/PHAGE_FINAL_PAPER/MODELS/CLEAN/caduceus/outputs/pretrain/hg38/caduceus-ps_seqlen-4k_d_model-256_n_layer-4_lr-8e-3/model_config.json"
-#PRETRAINED_PATH="/uufs/chpc.utah.edu/common/home/u1323098/sundar-group-space2/PHAGE_FINAL_PAPER/MODELS/CLEAN/caduceus/outputs/pretrain/hg38/caduceus-ps_seqlen-4k_d_model-256_n_layer-4_lr-8e-3/checkpoints/last.ckpt"
-#DISPLAY_NAME="caduceus_ps_char_4k_d256_4L"
+LOG_DIR="../watch_folder/phage_cv10/caduceus"
+CONFIG_PATH=$(realpath "../outputs/pretrain/hg38/debug-bakterion-caduceus_ps_seqlen-4k_d_model-256_n_layer-4_lr-8e-4/model_config.json")
+PRETRAINED_PATH=$(realpath "../outputs/pretrain/hg38/debug-bakterion-caduceus_ps_seqlen-4k_d_model-256_n_layer-4_lr-8e-4/checkpoints/last.ckpt")
+DISPLAY_NAME="bakterion_caduceus_ps_char_4k_d256_4L"
 
-#MODEL="caduceus"
-#MODEL_NAME="dna_embedding_caduceus"
-#CONJOIN_TRAIN_DECODER="true"  # Use this in decoder to always combine forward and reverse complement channels
-#CONJOIN_TEST="false"
-#RC_AUGS=( "false" )
-#LRS=( "1e-3" "2e-3" )
+MODEL="caduceus"
+MODEL_NAME="dna_embedding_caduceus"
+CONJOIN_TRAIN_DECODER="true"  # Use this in decoder to always combine forward and reverse complement channels
+CONJOIN_TEST="false"
+RC_AUGS=( "false" )
+LRS=( "1e-3" "2e-3" )
 
 mkdir -p "${LOG_DIR}"
 export_str="ALL,CONFIG_PATH=${CONFIG_PATH},PRETRAINED_PATH=${PRETRAINED_PATH},DISPLAY_NAME=${DISPLAY_NAME},MODEL=${MODEL},MODEL_NAME=${MODEL_NAME},CONJOIN_TRAIN_DECODER=${CONJOIN_TRAIN_DECODER},CONJOIN_TEST=${CONJOIN_TEST}"
-#for TASK in "dummy_mouse_enhancers_ensembl" "demo_coding_vs_intergenomic_seqs" "demo_human_or_worm" "human_enhancers_cohn" "human_enhancers_ensembl" "human_ensembl_regulatory" "human_nontata_promoters" "human_ocr_ensembl" "drosophila_enhancers_stark"; do
-#for TASK in "drosophila_enhancers_stark"; do
-#for TASK in "emp_H3"; do
-for TASK in "emp_H3" "emp_H3K14ac" "emp_H3K36me3" "emp_H3K4me1" "emp_H3K4me2" "emp_H3K4me3" "emp_H3K79me3" "emp_H3K9ac" "emp_H4" "emp_H4ac" "human_tf_0" "human_tf_1" "human_tf_2" "human_tf_3" "human_tf_4" "mouse_0" "mouse_1" "mouse_2" "mouse_3" "mouse_4" "prom_300_all" "prom_300_notata" "prom_300_tata" "prom_core_all" "prom_core_notata" "prom_core_tata" "splice_reconstructed" "virus_covid"; do
+for TASK in "phage_fragment_inphared" "phage_fragment_phaster"; do
   for LR in "${LRS[@]}"; do
     for BATCH_SIZE in 128 256; do
       for RC_AUG in "${RC_AUGS[@]}"; do
